f640567ede57fd786d112b5c101150995eca8a75
Nafishbinislam4 <94175329+Nafishbinislam4@users.noreply.github.com>
2026-01-08 00:44:12 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f model.gguf ]; then
   echo "Downloading AI model (TinyLlama Q2)..."
-  wget https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q2_K_M.gguf -O model.gguf
+  wget https://drive.google.com/file/d/1qi4dQG77fqZUz_SXNnX7GQ2JX80vEJ0D/view?usp=sharing
 fi
 
 uvicorn main:app --host 0.0.0.0 --port 10000
